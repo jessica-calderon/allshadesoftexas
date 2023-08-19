25b3f63a1b632d446d98b0fcc545224823a8de37
Jessica Calderon <calderonjessica13@yahoo.com>
2023-08-18 23:24:49 -0500

    button routes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
-import img1 from "../../assets/solar/img1.png"
+import img1 from "../../assets/solar/img1.png";
 import img2 from "../../assets/solar/img2.png";
 import img3 from "../../assets/solar/img3.png";
 import img4 from "../../assets/solar/img4.png";
 import img5 from "../../assets/solar/img5.png";
+import { useNavigate } from "react-router-dom";
 
 function Hero() {
     const images = [img1, img2, img3, img4, img5];
     const [currentImage, setCurrentImage] = useState(0);
+    const navigate = useNavigate();
+
+    const handleContactUsClick = () => {
+        navigate("/contact-us");
+    };
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -20,16 +26,18 @@ function Hero() {
     return (
         <div>
             {/* Slideshow */}
-            <div className="relative h-96 overflow-hidden">
-                <div className="relative w-full h-full">
-                    <img src={images[currentImage]} alt="Slideshow Image" className="w-full h-full object-cover" />
-                    <div className="absolute inset-0 bg-black opacity-70"></div>
+            <div className='relative h-96 overflow-hidden'>
+                <div className='relative w-full h-full'>
+                    <img src={images[currentImage]} alt='Slideshow Image' className='w-full h-full object-cover' />
+                    <div className='absolute inset-0 bg-black opacity-70'></div>
                 </div>
 
                 {/* Overlay Text and Button */}
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
-                    <h2 className="text-3xl mb-4">All Shades of Texas</h2>
-                    <button className="hover:bg-[rgb(251,175,68)] p-2 rounded">Contact Us</button>
+                <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
+                    <h2 className='text-3xl mb-4'>All Shades of Texas</h2>
+                    <button onClick={handleContactUsClick} className='hover:bg-[rgb(251,175,68)] p-2 rounded'>
+                        Contact Us
+                    </button>
                 </div>
             </div>
         </div>
